e4db3825e58a2579850cd89d37e24a26a4160a8b
gloria <gloria.gao115@gmail.com>
2021-11-29 19:23:28 +1100

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name)
+}
+destructivelyAppendCat('Ralph');
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+destructivelyPrependCat('Bob');
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop();
+}
+destructivelyRemoveLastCat();
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift();
+}
+destructivelyRemoveFirstCat();
+
+const appendCat = (name) => {
+    const newCats = [...cats, name];
+    return newCats;
+}
+appendCat('Broom');
+
+const prependCat = name => {
+    const newCats2 = [name, ...cats];
+    return newCats2;
+}
+prependCat('Arnold')
+
+const removeLastCat = () => {
+    const newCats3 = cats.slice(0, -1);
+    return newCats3;
+}
+removeLastCat();
+
+const removeFirstCat = (name) =>  {
+    const newCats4 = cats.slice(1);
+    return newCats4;
+}
+removeFirstCat();
